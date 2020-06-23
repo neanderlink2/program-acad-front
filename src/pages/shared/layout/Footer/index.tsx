0616c526d543fbe31eb45e295338a3c2a115cddd
Neander Wendel <neanderlink123@gmail.com>
2020-06-23 11:46:06 -0300

    Alteração no footer

--- src/pages/shared/layout/Footer/index.tsx
+++ src/pages/shared/layout/Footer/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { SpacedBox, Logo, Credits } from './styles';
+import React from "react";
+import logoIFB from "../../../../assets/logo_ifb.png";
+import { Credits, Logo, SpacedBox } from "./styles";
 
-import logoIFB from '../../../../assets/logo_ifb.png';
 
 export const Footer = () => {
-    return (
-        <SpacedBox component="footer">
-            <Logo src={logoIFB} alt="Logo do IFB (Instituto Federal de Brasília)" />
-            <Credits>Program.Acad - {new Date().getFullYear()}</Credits>
-        </SpacedBox>
-    )
-}
+  return (
+    <footer>
+      <SpacedBox>
+        <Logo src={logoIFB} alt="Logo do IFB (Instituto Federal de Brasília)" />
+        <Credits>Program.Acad - {new Date().getFullYear()}</Credits>
+      </SpacedBox>
+    </footer>
+  );
+};
