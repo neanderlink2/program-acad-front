33e56fe85791c5a110b3b7007229f945a83fc028
Neander Wendel <neanderlink123@gmail.com>
2020-06-28 14:33:55 -0300

    Correção do problema ao adicionar nickname

--- src/modules/account/sagas.ts
+++ src/modules/account/sagas.ts
@@ -1,8 +1,19 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
-import { CREATE_EXTERNAL_USER_REQUESTED, CreateExternalUserAction, CREATE_INTERNAL_USER_REQUESTED, CreateInternalUserAction } from './types';
-import { postRequest, formatErrors } from '../../api/index';
-import { confirmarUsuarioExternoCriado, informarErrosUsuarioCriado, informarErrosUsuarioInternoCriado, confirmarUsuarioInternoCriado } from './actions';
-import { history } from '../../configs/middlewares';
+import { all, call, put, takeLatest } from "redux-saga/effects";
+import { formatErrors, postRequest } from "../../api/index";
+import { updateUser } from "../../configs/firebaseConfig";
+import { history } from "../../configs/middlewares";
+import {
+    confirmarUsuarioExternoCriado,
+    confirmarUsuarioInternoCriado,
+    informarErrosUsuarioCriado,
+    informarErrosUsuarioInternoCriado
+} from "./actions";
+import {
+    CreateExternalUserAction,
+    CreateInternalUserAction,
+    CREATE_EXTERNAL_USER_REQUESTED,
+    CREATE_INTERNAL_USER_REQUESTED
+} from "./types";
 
 // type UsuarioCriado = {
 //     id: string,
@@ -16,27 +27,30 @@ import { history } from '../../configs/middlewares';
 // }
 
 export function* criarUsuarioExterno({ payload }: CreateExternalUserAction) {
-    try {
-        const body = { nickname: payload };
-        yield call(postRequest, `/v1/account`, body);
-        yield put(confirmarUsuarioExternoCriado());
-        history.push("/turmas");
-    } catch (error) {
-        yield put(informarErrosUsuarioCriado(formatErrors(error)));
-    }
+  try {
+    const body = { nickname: payload };
+    yield call(postRequest, `/v1/account`, body);
+    yield put(confirmarUsuarioExternoCriado());
+    yield call(updateUser);
+    setTimeout(() => {
+      history.push("/turmas");
+    }, 1000);
+  } catch (error) {
+    yield put(informarErrosUsuarioCriado(formatErrors(error)));
+  }
 }
 
 export function* criarUsuarioInterno({ payload }: CreateInternalUserAction) {
-    try {
-        yield call(postRequest, `/v1/account`, payload);
-        yield put(confirmarUsuarioInternoCriado());
-        history.push("/login");
-    } catch (error) {
-        yield put(informarErrosUsuarioInternoCriado(formatErrors(error)));
-    }
+  try {
+    yield call(postRequest, `/v1/account`, payload);
+    yield put(confirmarUsuarioInternoCriado());
+    history.push("/login");
+  } catch (error) {
+    yield put(informarErrosUsuarioInternoCriado(formatErrors(error)));
+  }
 }
 
 export const accountSaga = all([
-    takeLatest(CREATE_EXTERNAL_USER_REQUESTED, criarUsuarioExterno),
-    takeLatest(CREATE_INTERNAL_USER_REQUESTED, criarUsuarioInterno)
+  takeLatest(CREATE_EXTERNAL_USER_REQUESTED, criarUsuarioExterno),
+  takeLatest(CREATE_INTERNAL_USER_REQUESTED, criarUsuarioInterno),
 ]);
