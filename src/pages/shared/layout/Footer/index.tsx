a52c917023061ba1f1b91d3cd6fbb81c26dc81c4
Neander Wendel <neanderlink123@gmail.com>
2020-09-27 20:24:42 -0300

    Remoção da imagem da logo do IFB

--- src/pages/shared/layout/Footer/index.tsx
+++ src/pages/shared/layout/Footer/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import logoIFB from "../../../../assets/logo_ifb.png";
-import { Credits, Logo, SpacedBox } from "./styles";
+import { Credits, SpacedBox } from "./styles";
 
 
 export const Footer = () => {
   return (
     <footer>
-      <SpacedBox>
-        <Logo src={logoIFB} alt="Logo do IFB (Instituto Federal de Brasília)" />
+      <SpacedBox>        
         <Credits>Program.Acad - {new Date().getFullYear()}</Credits>
       </SpacedBox>
     </footer>
